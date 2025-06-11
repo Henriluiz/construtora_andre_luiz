e4c9207671d6c55d462caf8a76c07602222dc717
Henriluiz <luizao.lids@gmail.com>
2025-06-11 17:23:00 -0300

    cadastro mobile pré-ajustado

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
+async function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 function abrirEmNovaAba(url) {
   window.open(url, '_blank');
+}
+
+async function animacaoEmBotton() {
+  // Seleciona o elemento com o ID "meuElemento"
+  const meuElemento = document.getElementById("confirmar");
+  
+  // Adiciona a classe "novaClasse" ao elemento
+  meuElemento.classList.add("spinner-border spinner-border-sm");
+  sleep(3000)
+  // Para remover uma classe, pode usar o método classList.remove()
+  meuElemento.classList.remove("spinner-border spinner-border-sm");
 }
